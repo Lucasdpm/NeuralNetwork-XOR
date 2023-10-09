7153e2cb51dbf6cb0d98138d54716f34475dbebb
LUCAS DAL POZZO <l.pozzo@edu.pucrs.br>
2023-10-09 00:23:53 -0300

    weights as outpuBias initialized

--- neuralNetwork.c
+++ neuralNetwork.c
@@ -7,9 +7,14 @@
 #define NUM_OUTPUT 1
 #define NUM_TRAINING_SET 4
 
+
 double init_weights();
+
 double sigmoid(double x);
 double dSigmoid(double x);
+
+void shuffle(int *array, size_t n);
+
 
 int main(void) {
 
@@ -35,10 +40,29 @@ int main(void) {
                                                             {1.0f},
                                                             {0.0f}};
     
+    // init all hidden weights
+    for (int i = 0; i < NUM_INPUT; i++) {
+        for ( int j = 0; j < NUM_HIDDEN_NODES; j++) {
+            hiddenWeights[i][j] = init_weights();
+        }
+    }
+
+    // init all output weights
+    for (int i = 0; i < NUM_HIDDEN_NODES; i++) {
+        for ( int j = 0; j < NUM_OUTPUT; j++) {
+            outputWeights[i][j] = init_weights();
+        }
+    }
+
+    // init all output bias
+    for (int i = 0; i < NUM_OUTPUT; i++) {
+        outputLayerBias[i] = init_weights();
+    }
     
 }
 
-// initializes the wheights with random numbers between 0 and 1
+
+// initializes the weights with random numbers between 0 and 1
 double init_weights() {
     // returns "randomNumber" / "maximumRandomNumber"
     return ((double) rand()) / ((double) RAND_MAX);
@@ -51,4 +75,20 @@ double sigmoid(double x) {
 // derivative of sigmoid function
 double dSigmoid(double x) {
     return x * (1 - x);
+}
+
+// shuffle the original array of the data set, 
+// 'n' stands for the size(bytes) of the training data
+void shuffle(int *array, size_t n) {
+
+    if (n < 1) {
+        size_t i;
+        for (i = 0; i < (n - 1); i++) {
+            size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
+
+            int t = array[j];
+            array[j] = array[i];
+            array[i] = t;
+        }
+    }
 }
