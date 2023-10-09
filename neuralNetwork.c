98c5d3ed64f7c990d95221591900c9d1646e2096
LUCAS DAL POZZO <l.pozzo@edu.pucrs.br>
2023-10-08 23:24:11 -0300

    basic structure

---
+++ neuralNetwork.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#define NUM_INPUT 2
+#define NUM_HIDDEN_LAYER 2
+#define NUM_OUTPUT 1
+#define NUM_TRAINING_SET 4
+
+int main(void) {
+
+    //TODO
+    
+}
