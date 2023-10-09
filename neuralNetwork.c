a87d3b9b113af83691e08d47512aaa3e7d4c42f3
LUCAS DAL POZZO <l.pozzo@edu.pucrs.br>
2023-10-08 23:47:15 -0300

    adding major variables

--- neuralNetwork.c
+++ neuralNetwork.c
@@ -3,12 +3,33 @@
 #include <math.h>
 
 #define NUM_INPUT 2
-#define NUM_HIDDEN_LAYER 2
+#define NUM_HIDDEN_NODES 2
 #define NUM_OUTPUT 1
 #define NUM_TRAINING_SET 4
 
 int main(void) {
 
-    //TODO
+    // learning rate
+    const double lr = 0.1f;
+
+    double hiddenLayer[NUM_HIDDEN_NODES];
+    double outputLayer[NUM_OUTPUT];
+
+    double hiddenLayerBias[NUM_HIDDEN_NODES];
+    double outputLayerBias[NUM_OUTPUT];
+
+    double hiddenWeights[NUM_INPUT][NUM_HIDDEN_NODES];
+    double outputWeights[NUM_HIDDEN_NODES][NUM_OUTPUT];
+
+    double training_input[NUM_TRAINING_SET][NUM_INPUT] = {{0.0f, 0.0f},
+                                                          {1.0f, 0.0f},
+                                                          {0.0f, 1.0f},
+                                                          {1.0f, 1.0f}};
+
+    double training_output[NUM_TRAINING_SET][NUM_OUTPUT] = {{0.0f},
+                                                            {1.0f},
+                                                            {1.0f},
+                                                            {0.0f}};
+    
     
 }
